130a561748d75f5c3185f757199b8fefa9686b01
Reyes1234 <50181399+Reyes1234@users.noreply.github.com>
2019-05-04 17:51:47 -0400

    Calling the function
    I was seeing if I could add the array inside the function then call it.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # ES2015
 
-Code to be run 
+Code to be run
+How do you take an array as a first parameter?
+Isn't a set the opposite of an array
+If an array is ordered and a set is unordered
--- test.js
+++ test.js
@@ -1,7 +1,12 @@
-function myset_add(array){
-  let test_set = [1,2,3];
-}
+// function myset_add(array){
+//   let test_set = [1,2,3];
+// }
+//
+// console.log(myset_add);
 
-console.log(myset_add);
+//I don't think the function was called correctly
 
-//I don't think the function was called correctly 
+const set 1 = new Set ([1,2,3,4,5]);
+console.log(set1.has(1));
+ console.log(set1.has(5));
+  console.log(set1.has(6));
