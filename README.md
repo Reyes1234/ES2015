ecf101600a0d67ed7ff32f70f154741852b94fca
Reyes1234 <50181399+Reyes1234@users.noreply.github.com>
2019-05-15 17:58:23 -0400

    Check if value is in set
    Update

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Code to be run
 How do you take an array as a first parameter?
 Isn't a set the opposite of an array
 If an array is ordered and a set is unordered
+I can use arr.forEach
--- run.js
+++ run.js
@@ -1,13 +1,36 @@
-function myset_add() {
-   this.myset_addArray = [];
-}
-myset_add.prototype.multiply = function(arr) {
-  arr.forEach(function(value) {
-    this.myset_addArray.push(value * value);
-  }, this);
-};
+let myArray = ['1', '2', '3'];
+console.log(myArray);
 
-const obj = new myset_add();
-obj.multiply([1,2,3]);
-console.log(obj.myset_addArray);
-// this works!
+
+set_add(myArray, '3');
+function set_add(theArray, theValue){
+  for(let i = 0; i < theArray.length; i++){
+    console.log(theArray[i]);
+    console.log(theValue);
+if(theArray == theValue){
+  console.log('return myArray');
+} else {
+  console.log('dont return myArray');
+}
+let new_set = [4,5,6];
+let othernew_set = [3,8,9];
+    }
+    myArray.push(theValue);
+    return myArray;
+  }
+  function myset_remove(theArray, remove_value){
+
+   test_set = [1,2,3];
+   new_set = myset_remove(test_set, 3);
+   if ( ! arrays_equal(new_set.sort(), [1,2]) ) {
+     console.log("Remove failed");
+   }
+  }
+  mySet = [1,2,3,4,5,6,8,9]
+  testValue = 12
+   mySet["some string value"] = true;
+   if (testValue in mySet) {
+     console.log(testValue + " is in the set");
+   } else {
+     console.log(testValue + " is not in the set");
+   }
